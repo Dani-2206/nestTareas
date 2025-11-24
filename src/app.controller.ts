18cfe4e9bf186f93e25cc33fc7e19c03aa42b205
unknown <dlandero.alexander@gmail.com>
2025-11-24 14:15:09 -0300

    Arreglos a la pagina inmicio

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,8 +7,10 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('')
+  @Get()
   getHello()   {
-    return "OLA";
+    return {
+      message: "Bienvenido a la API de Gestión de Tareas"
+  }
   }
 }
